126fd46c20688bdb7179ebf55379535e74f5925a
Timothy Duggan <timothyduggan@Timothys-MacBook-Pro.local>
2019-05-29 23:22:16 -0400

    Working on modularizing code. TODO: create a class that will build each level of the table, so I can build it for year 1, 5, 10, 15, and 30.

--- get_mortgage.py
+++ get_mortgage.py
@@ -14,53 +14,54 @@ fv = 0
 running_int_total   = 0
 running_pri_total   = 0
 running_tax_total   = 0
-taxes               = []
-interest            = []
-principal           = []
 new_value           = []
-net_rent            = []
-effective_rent      = []
-prop_tax            = []
 insurance           = []
-NOI                 = []
+NOI                 = []    #
+mortgage            = []
+cash_flow           = []    #
+tax_ded             = []    #
+equity_accrued      = []
+
+k = 0
 
 for per in range(nper):
     
 
     if per !=0 and per % 12 == 0: 
-        nv                      =   pv * 1.07
+        nv                      =   pv * (1.07)**(per/12)
         ins_temp                =   nv * .01
         net_rent_temp           =   st.RENT * (1.04)**(per/12)
         eff_rent_temp           =   net_rent_temp * .9
         prop_temp               =   st.PROP_TAX_RATE * nv
-        NOI_temp                =   eff_rent_temp - (net_rent_temp*.1) - ins_temp - prop_temp
+        mrg_temp                =   running_int_total + running_pri_total
+        ert                     =   eff_rent_temp - (net_rent_temp*.1)
+        ipr                     =   ins_temp + prop_temp
+        
 
-        # print("Eff_rent_temp is " + str(eff_rent_temp) + " for year " + str(per))
+        NOI_temp                =   ert - ipr
+        cflow_temp              =   NOI_temp - mrg_temp - prop_temp
+        taxd_temp               =   ( st.DEPR + running_int_total ) * trate
 
-        net_rent                +=  [net_rent_temp]
-        effective_rent          +=  [eff_rent_temp]
+        equity_accrued          +=  nv * k / pv         
+        tax_ded                 +=  [taxd_temp]
         NOI                     +=  [NOI_temp]
         new_value               +=  [nv]
         insurance               +=  [ins_temp]
-        prop_tax                +=  [prop_temp]
+        cash_flow               +=  [cflow_temp]
 
-        interest            +=  [running_int_total]
-        principal           +=  [running_pri_total]
-        taxes               +=  [running_tax_total]
-        running_int_total   =   0
-        running_pri_total   =   0
+        # interest                +=  [running_int_total]
+        # principal               +=  [running_pri_total]
+        # taxes                   +=  [running_tax_total]
+        running_int_total       =   0
+        running_pri_total       =   0
         
     p                   =   -np.ppmt(rate, per, nper, pv)
     i                   =   -np.ipmt(rate, per, nper, pv)
     running_int_total   +=  i
     running_pri_total   +=  p
+    k                   +=  p
     running_tax_total   +=  i * trate
 
-
-# NOI                 =   effective_rent - (net_rent*.1) - insurance - prop_tax
-# mortgage            =   principal[j] + interest[j]
-# cash_flow           =   NOI - mortgage - prop_tax
-# tax_ded             =   ( st.DEPR + interest[j] ) * trate
 # new_value           =   pv * (1.07)**j
 # equity_accrued      =   new_value * k / pv 
 # cash_in             =   st.REHAB + st.DOWN_PAYMENT + insurance
--- logic.py
+++ logic.py
@@ -4,41 +4,29 @@ import numpy as np
 import get_mortgage as gm
 
 pv      =   st.HOUSE_PRICE
-rent    =   st.RENT
-trate   =   st.TAX_RATE
 
-interest = gm.interest
-principal = gm.principal
-new_value = gm.new_value
 
 j = st.j
 
-k = 0
-for per in range(j):
-    k += principal[per] 
+equity_accrued      =   gm.new_value[j]
 
 ###############
 ## Equations ## 
 ###############
-net_rent            =   gm.net_rent[j]
-net_taxes           =   gm.taxes[j]
-effective_rent      =   gm.effective_rent[j]
-prop_tax            =   gm.prop_tax[j]
-insurance           =   gm.insurance[j]
-NOI                 =   effective_rent - (net_rent*.1) - insurance - prop_tax
-# NOI                 =   gm.NOI[j]
-mortgage            =   principal[j] + interest[j]
-cash_flow           =   NOI - mortgage - prop_tax
-tax_ded             =   ( st.DEPR + interest[j] ) * trate
-new_value           =   pv * (1.07)**j
-equity_accrued      =   new_value * k / pv 
+insurance           =   gm.insurance[j]         
+NOI                 =   gm.NOI[j]               #
+cash_flow           =   gm.cash_flow[j]         #
+tax_ded             =   gm.tax_ded[j]
+
+# tax_ded             =   ( st.DEPR + interest[j] ) * trate
+
+
 cash_in             =   st.REHAB + st.DOWN_PAYMENT + insurance
 ROI                 =   100 * NOI / cash_in
 cash_on_cash        =   100 * NOI / cash_in
 cap_rate            =   100 * NOI / pv
 
 # I want an array of 
-# # cash flow per year
 # # Tax deduction per year
 # # equity accrued per year
 # # NOI per year
