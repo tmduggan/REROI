969acd9cc797b8b841038b513bdbfac6187bf616
Timothy Duggan <timothyduggan@Timothys-MacBook-Pro.local>
2019-05-30 23:11:04 -0400

    A ton of changes, removed logic.py after moving all of its code into classes within get_mortgage.py. TODO: Clean up ROI.py as there is some way to turn this into a function. I just cant figure out how rignt now. TODO: Rename ROI to main. TODO: GUI

--- ROI.py
+++ ROI.py
@@ -2,40 +2,78 @@
 
 import numpy as np
 from astropy.table import Table
-import logic as l
+from astropy.table import Column
+import get_mortgage as gm
 
-brk_line    =   "-------"
-cf_str      =   "$" +   "{:,.2f}".format(l.cash_flow)
-td_str      =   "$" +   "{:,.2f}".format(l.tax_ded)
-ea_str      =   "$" +   "{:,.2f}".format(l.equity_accrued)
-NOI_str     =   "$" +   "{:,.2f}".format(l.NOI)
-cin_str     =   "$" +   "{:,.2f}".format(l.cash_in)
-coc_str     =           "{:,.2f}".format(l.cash_on_cash)    + "%"
-Cap_str     =           "{:,.2f}".format(l.cap_rate)        + "%"
-ROI_str     =           "{:,.2f}".format(l.ROI)             + "%"
 
 a   =   ["Cash Flow"]
 a   +=  ["Tax Deduction"]
 a   +=  ["Equity Accrued"]
 a   +=  ["NOI"]
-a   +=  ["Cash in"]
-a   +=  [brk_line]
-a   +=  ["Cash on cash"]
-a   +=  ["Cap Rate"]
-a   +=  ["ROI"]
+
+cf_str      =   "$" +   "{:,.2f}".format(   gm.cash_flow[1]         )
+td_str      =   "$" +   "{:,.2f}".format(   gm.tax_ded[1]           )
+ea_str      =   "$" +   "{:,.2f}".format(   gm.equity_accrued[1]    )
+NOI_str     =   "$" +   "{:,.2f}".format(   gm.NOI[1]               )
 
 b   =   [cf_str]
 b   +=  [td_str]
 b   +=  [ea_str]
 b   +=  [NOI_str]
-b   +=  [cin_str]
-b   +=  [brk_line]
-b   +=  [coc_str]
-b   +=  [Cap_str]
-b   +=  [ROI_str]
 
-t = Table([a, b], names=('Annual', l.j))
+cf_str      =   "$" +   "{:,.2f}".format(   gm.cash_flow[5]         )
+td_str      =   "$" +   "{:,.2f}".format(   gm.tax_ded[5]           )
+ea_str      =   "$" +   "{:,.2f}".format(   gm.equity_accrued[5]    )
+NOI_str     =   "$" +   "{:,.2f}".format(   gm.NOI[5]               )
+
+c   =   [cf_str]
+c   +=  [td_str]
+c   +=  [ea_str]
+c   +=  [NOI_str]
+
+cf_str      =   "$" +   "{:,.2f}".format(   gm.cash_flow[15]         )
+td_str      =   "$" +   "{:,.2f}".format(   gm.tax_ded[15]           )
+ea_str      =   "$" +   "{:,.2f}".format(   gm.equity_accrued[15]    )
+NOI_str     =   "$" +   "{:,.2f}".format(   gm.NOI[15]               )
+
+d   =   [cf_str]
+d   +=  [td_str]
+d   +=  [ea_str]
+d   +=  [NOI_str]
+
+
+cin_str     =   "$" +   "{:,.2f}".format(   gm.cash_in      )
+coc_str     =           "{:,.2f}".format(   gm.cash_on_cash  )    + "%"
+Cap_str     =           "{:,.2f}".format(   gm.cap_rate      )        + "%"
+ROI_str     =           "{:,.2f}".format(   gm.ROI           )             + "%"
+
+# d/e table will be one time calculations
+x   =  ["Cash in"]
+x   +=  ["Cash on cash"]
+x   +=  ["Cap Rate"]
+x   +=  ["ROI"]
+
+y   =  [cin_str]
+y   +=  [coc_str]
+y   +=  [Cap_str]
+y   +=  [ROI_str]
+
+
+
+
+# b   =   [cf_str]
+# b   +=  [td_str]
+# b   +=  [ea_str]
+# b   +=  [NOI_str]
+
+
+t = Table([a, b, c, d], names=('Annual', '1', '5', '15'))
+u = Table([x, y], names=('One Time Calcuations', 'Total'))
+print('\n')
+print(u)
+print('\n')
 print(t)
+
 
 # Now I want a table to show the numbers at year 1, 5, 10, and 20
 # 
--- get_mortgage.py
+++ get_mortgage.py
@@ -14,57 +14,72 @@ fv = 0
 running_int_total   = 0
 running_pri_total   = 0
 running_tax_total   = 0
-new_value           = []
-insurance           = []
 NOI                 = []    #
-mortgage            = []
 cash_flow           = []    #
 tax_ded             = []    #
 equity_accrued      = []
 
-k = 0
 
-for per in range(nper):
-    
 
-    if per !=0 and per % 12 == 0: 
-        nv                      =   pv * (1.07)**(per/12)
-        ins_temp                =   nv * .01
-        net_rent_temp           =   st.RENT * (1.04)**(per/12)
-        eff_rent_temp           =   net_rent_temp * .9
-        prop_temp               =   st.PROP_TAX_RATE * nv
-        mrg_temp                =   running_int_total + running_pri_total
-        ert                     =   eff_rent_temp - (net_rent_temp*.1)
-        ipr                     =   ins_temp + prop_temp
+
+class get_equity:
+    k = 0
+    for per in range (nper):
+        if per !=0 and per % 12 == 0:
+            nv              =   pv * (1.07)**(per/12)
+            equity_accrued  +=  [nv * k / pv]
         
+        p                   =   -np.ppmt(rate, per, nper, pv)
+        k                   +=  p
 
-        NOI_temp                =   ert - ipr
-        cflow_temp              =   NOI_temp - mrg_temp - prop_temp
-        taxd_temp               =   ( st.DEPR + running_int_total ) * trate
+class get_taxd:
+    for per in range (nper):
+        if per !=0 and per % 12 == 0:
+            taxd_temp               =   ( st.DEPR + running_int_total ) * trate
+            tax_ded                 +=  [taxd_temp]
+            running_int_total       =   0
+        ##
+        i                   =   -np.ipmt(rate, per, nper, pv)
+        running_int_total   +=  i
 
-        equity_accrued          +=  nv * k / pv         
-        tax_ded                 +=  [taxd_temp]
-        NOI                     +=  [NOI_temp]
-        new_value               +=  [nv]
-        insurance               +=  [ins_temp]
-        cash_flow               +=  [cflow_temp]
+class get_NOI:
+    for per in range (nper):
+        if per !=0 and per % 12 == 0:
+            # new value of house
+            nv                      =   pv * (1.07)**(per/12)
+            # property insurance 1%
+            ins_temp                =   nv * .01
+            # property tax 1.69%
+            prop_temp               =   st.PROP_TAX_RATE * nv
+            # rent with appropriate annual increase
+            net_rent_temp           =   st.RENT * (1.04)**(per/12)
+            # effective rent after deducting 10% for repairs
+            eff_rent_temp           =   net_rent_temp * .9
 
-        # interest                +=  [running_int_total]
-        # principal               +=  [running_pri_total]
-        # taxes                   +=  [running_tax_total]
-        running_int_total       =   0
-        running_pri_total       =   0
+            # intermediate values with 10% off rent for management
+            ert                     =   eff_rent_temp - (net_rent_temp*.1)
+            ipr                     =   ins_temp + prop_temp
         
-    p                   =   -np.ppmt(rate, per, nper, pv)
-    i                   =   -np.ipmt(rate, per, nper, pv)
-    running_int_total   +=  i
-    running_pri_total   +=  p
-    k                   +=  p
-    running_tax_total   +=  i * trate
+            NOI                     +=  [ert - ipr]
 
-# new_value           =   pv * (1.07)**j
-# equity_accrued      =   new_value * k / pv 
-# cash_in             =   st.REHAB + st.DOWN_PAYMENT + insurance
-# ROI                 =   100 * NOI / cash_in
-# cash_on_cash        =   100 * NOI / cash_in
-# cap_rate            =   100 * NOI / pv
+class get_cflow:
+    for per in range (nper):
+        if per !=0 and per % 12 == 0:
+            # total mortgage
+            mrg_temp                =   running_int_total + running_pri_total
+            iter_i                  =   per/12 - 1
+            NOI_temp                =   NOI[ iter_i ]
+            cash_flow               +=  [NOI_temp - mrg_temp]
+
+            # reset intermediate values
+            running_int_total       =   0
+            running_pri_total       =   0
+        p                   =   -np.ppmt(rate, per, nper, pv)
+        i                   =   -np.ipmt(rate, per, nper, pv)
+        running_int_total   +=  i
+        running_pri_total   +=  p
+
+cash_in             =   st.REHAB + st.DOWN_PAYMENT + (st.HOUSE_PRICE_FULL * .01)
+ROI                 =   100 * NOI[1] / cash_in
+cash_on_cash        =   100 * NOI[1] / cash_in
+cap_rate            =   100 * NOI[1] / st.HOUSE_PRICE
